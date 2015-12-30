63413af32f2f3a9e68fe660b4050f074471b1c80
Layon de Souza Onofre <layonsonofre@gmail.com>
2015-12-30 13:07:18 -0200

    Adicionando um funcionário no banco

---
+++ banco/insertions.sql
@@ -0,0 +1,84 @@
+/* Database export results for db dbSisgraf */
+
+/* Preserve session variables */
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS;
+SET FOREIGN_KEY_CHECKS=0;
+
+/* Export data */
+
+/* data for Table Acab_OS_TS */
+
+/* data for Table Acabamento */
+
+/* data for Table Arquivo */
+
+/* data for Table ArquivoMatriz */
+
+/* data for Table ArquivoModelo */
+
+/* data for Table Arquivo_ArquivoMatriz */
+
+/* data for Table Carimbo */
+
+/* data for Table Categoria */
+
+/* data for Table Categoria_Material */
+
+/* data for Table Cor */
+
+/* data for Table Cor_Material */
+
+/* data for Table Cor_OrdemDeServico_TipoServico */
+
+/* data for Table Email */
+
+/* data for Table FormaImpressao */
+
+/* data for Table Formato */
+
+/* data for Table Fornecedor_Categoria */
+
+/* data for Table Funcionario */
+INSERT INTO `Funcionario` VALUES (1,"paulinho","71fb6e698e80dc0a7b032e8dfacee85f","padrao");
+
+/* data for Table GramaturaPapel */
+
+/* data for Table Material */
+
+/* data for Table MaterialUnidade */
+
+/* data for Table Material_TipoServico */
+
+/* data for Table Material_Vias */
+
+/* data for Table ModeloNotaFiscal */
+
+/* data for Table NotaFiscal */
+
+/* data for Table OrdemDeServico */
+
+/* data for Table OrdemDeServico_TipoServico */
+
+/* data for Table Papel */
+
+/* data for Table Pessoa */
+INSERT INTO `Pessoa` VALUES (1,"Paulo Eduardo Onofre","funcionarioAtivo",1,"404.988.888-27","48.130.023-5",NULL,NULL,NULL,NULL,NULL,"Rua dos Gerânios",205,"Esquina","18960-000","Jardim Brasil IV","SP","Bernardino de Campos");
+
+/* data for Table Pessoa_OrdemDeServico */
+
+/* data for Table QuantidadeCores */
+
+/* data for Table ServicoExterno */
+
+/* data for Table Telefone */
+
+/* data for Table TipoServico */
+
+/* data for Table TipoServico_Acabamento */
+
+/* data for Table TipoServico_Formato */
+
+/* data for Table Vias */
+
+/* Restore session variables to original values */
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
